0325a38c49c24b6c7bc2abafcb8f1f8d8ecaf71f
Daniel Rrapi <uzo1209@gmail.com>
2024-02-23 11:51:00 +0100

    Added JWT

---
+++ src/main/java/com/danielrrapi/u5d15/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.danielrrapi.u5d15.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
